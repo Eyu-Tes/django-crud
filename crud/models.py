9700e2942ce95429f95a27d9ddaa24e66164db0d
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2020-09-19 15:15:54 +0300

    added product model and form

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+class Product(models.Model):
+    # enum
+    class Categories(models.TextChoices):
+        INDOOR = 'Indoor', _('Indoor')
+        OUTDOOR = 'Outdoor', _('Outdoor')
+
+    name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    product_code = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.IntegerField()
+    category = models.CharField(max_length=10,
+                                choices=Categories.choices,
+                                default=Categories.INDOOR,
+                                blank=True, null=True)
+
+    class Meta:
+        db_table = 'product'
+        ordering = ['id']
--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .forms import ProductForm
+from .models import Product
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hy')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'crud/index.html', context=context)
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+    else:
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'crud/index.html', context=context)
+
+
+def edit_product(request):
+    pass
+
+
+def remove_product(request):
+    pass
